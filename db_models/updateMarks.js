9677799479818830c29206d4406608f9e6ad45a2
Vitalik <Vitaliksomeman@mail.ru>
2016-10-13 22:55:48 +0300

    add update marks

---
+++ db_models/updateMarks.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Виталий on 04.10.2016.
+ */
+module.exports.updateMarks = function(marks) {
+    
+    // внимание: удаления поля оценки не происходит
+
+    function updateMarks(aData, callback, error) {
+        var success = true;
+        aData.req.forEach(function (mark) {
+            marks.updateData(JSON.parse(mark), function(res) {}, function(err) {
+                success = false;
+            });
+        });
+        if (success) {
+            callback(true);
+        } else {
+            error(false);
+        }
+    }
+
+    return updateMarks;
+}
