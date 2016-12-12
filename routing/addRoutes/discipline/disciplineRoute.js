8c22533f6282ab393e998406fb952ae7f6222744
Vitalik <vitaliksomeman@mail.ru>
2016-12-12 08:23:46 +0300

    add some routes

---
+++ routing/addRoutes/discipline/disciplineRoute.js
@@ -0,0 +1,17 @@
+/**
+ * Created by vitalik on 12.12.16.
+ */
+module.exports.add = function(core) {
+    var handlerFactory = core.handlerFactory;
+    var db = core.db;
+
+    core.routes.push({
+        url: '/add/discipline',
+        handler: handlerFactory.createWithData(db.discipline.addData)
+    });
+
+    core.routes.push({
+        url: '/add/group_dis',
+        handler: handlerFactory.createWithData(db.discipline.addData)
+    });
+};
